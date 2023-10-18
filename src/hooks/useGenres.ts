4330c5baf36c44026d699fee6b163bc105b350f7
jwwals1 <129614828+jwwals1@users.noreply.github.com>
2023-10-18 13:43:59 -0500

    Refactor- create lookeup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -17,7 +17,7 @@ const useGenres = () => useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24hr
-    initialData: { count: genres.length, results: genres}
+    initialData: genres
 })
 
 export default useGenres;
