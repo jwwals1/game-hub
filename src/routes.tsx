5df141aae08b8eb2621b186dcd5e6e9bdd04b10c
jwwals1 <129614828+jwwals1@users.noreply.github.com>
2023-10-20 13:55:49 -0500

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetailPages from "./pages/GameDetailPages";
+import ErrorPage from "./pages/ErrorPAge";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPages /> },
